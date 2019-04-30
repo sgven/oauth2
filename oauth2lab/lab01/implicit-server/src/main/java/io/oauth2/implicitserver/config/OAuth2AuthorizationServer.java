8848b638e4afb3ffc255d8400a37e3d9075296ef
sgven <sgvenday@outlook.com>
2019-04-30 15:15:24 +0800

    简化模式

---
+++ oauth2lab/lab01/implicit-server/src/main/java/io/oauth2/implicitserver/config/OAuth2AuthorizationServer.java
@@ -0,0 +1,27 @@
+package io.oauth2.implicitserver.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * 授权服务器配置
+ */
+//@Configurable
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter{
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("clientapp")
+                .secret("12345")
+                .redirectUris("http://localhost:9001/callback")
+                // 授权码模式
+                .authorizedGrantTypes("authorization_code")
+                .scopes("read_userinfo","read_contacts");
+    }
+}
