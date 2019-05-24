95c300ff118c20add81db280b7916568b958448c
sgven <sgvenday@outlook.com>
2019-05-24 10:59:57 +0800

    oauth-jdbc

---
+++ oauth2lab/lab01-extend/authcode-server-jdbc/src/main/java/io/oauth2/authcodeserver/AuthcodeServerApplication.java
@@ -0,0 +1,13 @@
+package io.oauth2.authcodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcodeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcodeServerApplication.class, args);
+	}
+
+}
