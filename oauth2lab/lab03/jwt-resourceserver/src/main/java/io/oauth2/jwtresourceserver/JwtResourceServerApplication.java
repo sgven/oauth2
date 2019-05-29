cc5e7972e78c45b82b53dd6d84cc5fa44ce26923
sgven <sgvenday@outlook.com>
2019-05-29 11:56:47 +0800

    lab03

---
+++ oauth2lab/lab03/jwt-resourceserver/src/main/java/io/oauth2/jwtresourceserver/JwtResourceServerApplication.java
@@ -0,0 +1,12 @@
+package io.oauth2.jwtresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtResourceServerApplication.class, args);
+	}
+}
